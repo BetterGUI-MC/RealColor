afc01af79901858e13008ddf20655874aa801932
hsgamer <huynhqtienvtag@gmail.com>
2020-06-29 12:03:19 +0700

    change something

--- src/main/java/me/hsgamer/bettergui/realcolor/Main.java
+++ src/main/java/me/hsgamer/bettergui/realcolor/Main.java
@@ -3,9 +3,8 @@ package me.hsgamer.bettergui.realcolor;
 import java.util.concurrent.ThreadLocalRandom;
 import me.hsgamer.bettergui.lib.xseries.XMaterial;
 import me.hsgamer.bettergui.manager.VariableManager;
-import me.hsgamer.bettergui.object.GlobalVariable;
 import me.hsgamer.bettergui.object.addon.Addon;
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
 
 public final class Main extends Addon {
 
@@ -20,30 +19,12 @@ public final class Main extends Addon {
 
   @Override
   public void onEnable() {
-    GlobalVariable hexColorVariable = (offlinePlayer, s) -> {
-      if (s.length() != 6) {
-        return null;
-      }
-
-      try {
-        Integer.parseInt(s, 16);
-      } catch (NumberFormatException e) {
-        return null;
-      }
-
-      return convertHexToColor(s);
-    };
-    VariableManager.register("hcolor_", hexColorVariable);
-    VariableManager.register("#", hexColorVariable);
+    VariableManager.register("hcolor_", (offlinePlayer, s) -> convertHexToColor(s));
     VariableManager.register("hrainbow", (offlinePlayer, s) -> convertHexToColor(
         String.format("%06x", ThreadLocalRandom.current().nextInt(0xFFFFFF + 1))));
   }
 
   private String convertHexToColor(String hex) {
-    StringBuilder builder = new StringBuilder(ChatColor.COLOR_CHAR + "x");
-    for (char c : hex.toCharArray()) {
-      builder.append(ChatColor.COLOR_CHAR).append(c);
-    }
-    return builder.toString();
+    return String.valueOf(ChatColor.of("#" + hex));
   }
 }
